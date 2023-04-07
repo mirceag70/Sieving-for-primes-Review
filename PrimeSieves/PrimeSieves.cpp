1d8ccd72ba21a35636fd1c6228ae70cc55904940
mirceag70 <mircea.ghidarcea@gmail.com>
2023-04-07 21:07:35 +0300

    v02

--- PrimeSieves/PrimeSieves.cpp
+++ PrimeSieves/PrimeSieves.cpp
@@ -13,7 +13,7 @@ uint64_t Atkin(const uint64_t Nmax, bool vPrimes[], void*, void*);
 void Try357(const uint64_t Nmax);
 void TryBen(const uint64_t Nmax);
 
-constexpr auto LIMIT = 1'000'000;
+constexpr auto LIMIT = 1'000000'000;
 
 int main()
 {
@@ -21,22 +21,22 @@ int main()
     std::cout.imbue(mylocale);  // imbue global locale for thousands delimiter
 
 
-    Try_Sieve<uint64_t, int, int, LIMIT / 4>    // for LIMIT >= 100 !!!
-        (LIMIT, "Trial Division", &Trial_Division, true);
-    Try_Sieve<bool, int, int, LIMIT + 1>
-        (LIMIT, "Basic Eratosthenes", &Eratosthenes_Basic, true);
-    Try_Sieve<uint64_t, uint64_t, int32_t, LIMIT / 4, LIMIT / 4, LIMIT / 4>
-        (LIMIT, "Chartres G2 - Singleton 356", &Singleton_optimized311, true);
-    Try_Sieve<uint64_t, uint64_t, uint64_t, LIMIT + 1, LIMIT + 1, LIMIT + 1>
-        (LIMIT, "Mairson Standard", & Mairson_Standard, true);
-    Try_Sieve<uint8_t, int, int, LIMIT + 1>
-        (LIMIT, "Gale & Pratt", &GalePratt, true);
-    Try_Sieve<uint64_t, uint64_t, int, LIMIT + 1, LIMIT + 1>
-        (LIMIT, "Gries/Misra Standard", &GriesMisra_Standard, true);
-    Try_Sieve<uint64_t, int, int, LIMIT + 1000>
-        (LIMIT, "Pritchard", &SoP, true);
-    Try_Sieve<bool, int, int, LIMIT>
-        (LIMIT, "Basic Atkin", &Atkin, true);
+    //Try_Sieve<uint64_t, int, int, LIMIT / 4>    // for LIMIT >= 100 !!!
+    //    (LIMIT, "Trial Division", &Trial_Division, true);
+    //Try_Sieve<bool, int, int, LIMIT + 1>
+    //    (LIMIT, "Basic Eratosthenes", &Eratosthenes_Basic, true);
+    //Try_Sieve<uint64_t, uint64_t, int32_t, LIMIT / 4, LIMIT / 4, LIMIT / 4>
+    //    (LIMIT, "Chartres G2 - Singleton 356", &Singleton_optimized311, true);
+    //Try_Sieve<uint64_t, uint64_t, uint64_t, LIMIT + 1, LIMIT + 1, LIMIT + 1>
+    //    (LIMIT, "Mairson Standard", & Mairson_Standard, true);
+    //Try_Sieve<uint8_t, int, int, LIMIT + 1>
+    //    (LIMIT, "Gale & Pratt", &GalePratt, true);
+    //Try_Sieve<uint64_t, uint64_t, int, LIMIT + 1, LIMIT + 1>
+    //    (LIMIT, "Gries/Misra Standard", &GriesMisra_Standard, true);
+    //Try_Sieve<uint64_t, int, int, LIMIT + 1000>
+    //    (LIMIT, "Pritchard", &SoP, true);
+    //Try_Sieve<bool, int, int, LIMIT>
+    //    (LIMIT, "Basic Atkin", &Atkin, true);
     TryBen(LIMIT);
     Try357(LIMIT);
 
